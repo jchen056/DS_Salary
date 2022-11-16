95922950195910578ddccd514a028410714fc4d3
Jerry Chen <jchen056@citymail.cuny.edu>
2022-11-15 20:33:50 -0500

    diff approach

--- pages/I_O.py
+++ pages/I_O.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import re
-
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score 
 from sklearn.feature_extraction.text import TfidfVectorizer
