95af0ccfc8a79079f59b8a88a03702e846bb5672
Jerry Chen <jchen056@citymail.cuny.edu>
2022-12-02 16:47:40 -0500

    company

--- pages/visualization.py
+++ pages/visualization.py
@@ -8,7 +8,7 @@ import plotly.graph_objs as go
 import plotly.figure_factory as ff
 
 st.sidebar.markdown("# Visualization")
-tab1, tab2,tab4, tab3 = st.tabs(["Overview", "States","Sectors", "I/O"])
+tab1, tab2,tab4,tab5, tab3 = st.tabs(["Overview", "States","Sectors","Company Size/Revenue", "I/O"])
 with tab1:
     
     df=pd.read_csv('pages/DS_DA_BS.csv')
@@ -41,7 +41,7 @@ with tab1:
     df['states']=df['Location'].apply(state_extract)
     with st.expander("Click here to view the data"):
         st.markdown("Here is our data:")
-        st.dataframe(data=df[['Job Title','role','states','Sector','Company Name','mean salary']],width=None, height=None)
+        st.dataframe(data=df[['Job Title','role','states','Sector','Company Name','Size','Revenue','mean salary']],width=None, height=None)
 
     st.markdown("**DS,DA,BA Salaries Comparison**")
     DS_salaries=list(df[df['role']=='DS']['mean salary'])
@@ -152,6 +152,132 @@ with tab4:
     st.markdown('''
     * More jobs can be found in IT and Financial Industry
     * Biotech and IT have higher pay''')
+
+with tab5:
+    df['Revenue'] = df['Revenue'].replace('Unknown / Non-Applicable', None)
+    df['Revenue'] = df['Revenue'].str.replace('$', ' ')
+    df['Revenue'] = df['Revenue'].str.replace('(USD)', ' ')
+    df['Revenue'] = df['Revenue'].str.replace('(', ' ')
+    df['Revenue'] = df['Revenue'].str.replace(')', ' ')
+    df['Revenue'] = df['Revenue'].str.replace(' ', '')
+    df['Revenue'] = df['Revenue'].str.replace('2to5billion','2billionto5billion')
+    df['Revenue'] = df['Revenue'].str.replace('1to2billion','1billionto2billion')
+    df['Revenue'] = df['Revenue'].str.replace('5to10billion','5billionto10billion')
+    df['Revenue'] = df['Revenue'].replace('10+billion', '10billionto11billion')
+    df['Revenue'] = df['Revenue'].str.replace('Lessthan1million', '0millionto1million')
+    df['Revenue'] = df['Revenue'].str.replace('million', ' ')
+    df['Revenue'] = df['Revenue'].str.replace('billion', '000 ')
+    df=df[df['Revenue'].isin(['100to500 ', '500 to1000 ', '10000 to11000 ', '25to50 ',
+       '5000 to10000 ', '5to10 ', '50to100 ', '1000 to2000 ',
+       '2000 to5000 ', '0 to1 ', '1to5 ', '10to25 '])]
+    df=df[df['Size'].isin(['51 to 200 employees', '1001 to 5000 employees',
+       '501 to 1000 employees', '10000+ employees', '1 to 50 employees',
+       '201 to 500 employees', '5001 to 10000 employees'])]
+    
+    avg_revenues=['0 to1 ','1to5 ','5to10 ', '10to25 ',
+              '25to50 ','50to100 ','100to500 ', '500 to1000 ', 
+              '1000 to2000 ','2000 to5000 ',
+       '5000 to10000 ', '10000 to11000 ']
+    avg_sizes=['1 to 50 employees',
+            '51 to 200 employees', '201 to 500 employees',
+            '501 to 1000 employees','1001 to 5000 employees',
+            '5001 to 10000 employees', '10000+ employees']
+    df_size_revenue=pd.DataFrame(np.zeros((len(avg_revenues), len(avg_sizes) )), index=avg_revenues,columns=avg_sizes)
+    for i in avg_revenues:
+        for j in avg_sizes:
+            df_size_revenue.at[i,j]=len(df[
+            (df['Revenue']==i) & (df['Size']==j)])
+    fig=px.imshow(df_size_revenue,text_auto=True,
+    labels=dict(x="Company Size",y="Millions of $",color="Count"),
+    aspect="auto")
+    st.plotly_chart(fig)
+    # fig, ax = plt.subplots()
+    # im = ax.imshow(df_size_revenue,interpolation='nearest')
+
+    # ax.set_yticks(np.arange(len(avg_revenues)), labels=avg_revenues)
+    # ax.set_xticks(np.arange(len(avg_sizes)), labels=avg_sizes)
+    # plt.setp(ax.get_xticklabels(), rotation=90, ha="right",
+    #         rotation_mode="anchor")
+    
+    # plt.title('Company Size and Revenue(M $)')
+
+    # fig.colorbar(im, ax=ax)
+    # st.pyplot(fig,clear_figure=True)
+    def generate_stack_bar(avg_revenues,col_name):
+        df_revenues_DS=pd.DataFrame(
+                np.zeros((len(avg_revenues),3)),
+                index=avg_revenues,
+                columns=['DS','DA','BA'])
+        for i in avg_revenues:
+            for j in ['DS','DA','BA']:
+                df_revenues_DS.at[i,j]=len(
+                    df[(df[col_name]==i) & (df['role']==j)]
+                )
+
+        ax=df_revenues_DS.plot.bar(stacked=True)
+        plt.ylabel('Job Count')
+        fig1=ax.get_figure()
+        st.pyplot(fig1,clear_figure=True)
+    
+    
+
+    def generate_bar_charts(avg_revenues,col_name):
+        df_revenues_DS=pd.DataFrame(
+                np.zeros((len(avg_revenues),3)),
+                index=avg_revenues,
+                columns=['min salary','max salary','mean salary'])
+        for i in avg_revenues:
+            df_revenues_DS.at[i]=df[df[col_name]==i][['min salary','max salary','mean salary']].mean()
+        # st.dataframe(df_revenues_DS)
+        data_sector=df_revenues_DS
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+        x = data_sector.index,
+        y = data_sector['mean salary'],
+        name = 'Mean Salary'
+        ))
+        fig.add_trace(go.Bar(
+        x = data_sector.index,
+        y = data_sector['min salary'],
+        name = 'Minimum Salary'
+        ))
+        fig.add_trace(go.Bar(
+        x = data_sector.index,
+        y = data_sector['max salary'],
+        name = 'Maximum Salary'
+        ))
+        st.plotly_chart(fig, use_container_width=True)
+    def generate_df_salries(avg_revenues,col_name):
+        df_revenues_DS=pd.DataFrame(
+                np.zeros((len(avg_revenues),3)),
+                index=avg_revenues,
+                columns=['min salary','max salary','mean salary'])
+        for i in avg_revenues:
+            df_revenues_DS.at[i]=df[df[col_name]==i][['min salary','max salary','mean salary']].mean()
+        st.dataframe(df_revenues_DS)
+
+    c_rev,c_size=st.columns(2)
+    with c_rev:
+        st.markdown("Job Count by Company Revenue")
+        generate_stack_bar(avg_revenues,"Revenue")
+        generate_df_salries(avg_revenues,"Revenue")
+    with c_size:
+        st.markdown("Job Count by Company Size")
+        generate_stack_bar(avg_sizes,"Size")
+        generate_df_salries(avg_sizes,"Size")
+    st.markdown("---")
+    st.markdown("**Salaries by Company Revenue**")
+    generate_bar_charts(avg_revenues,"Revenue")
+    st.markdown("**Salaries by Company Size**")
+    generate_bar_charts(avg_sizes,"Size")
+    st.markdown("---")
+    st.markdown('''
+    * The higher the revenue, the bigger the company size.
+    * More jobs can be found in really big corporations(10 billion+ in revenue and 10000+ employees).
+    * Salaries about the same across companies of different revenues and sizes. Really big corporations(10 billion+ in revenue and 10000+ employees) offer more salaries.'''
+    )
+    
+
 with tab3:
     with st.expander("Click to see jobs by states"):
         options = st.multiselect(
